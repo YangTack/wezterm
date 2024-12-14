9c935f65e9afb63834ee92e63e46718a8e016f84
PeterYang <30444059+YangTack@users.noreply.github.com>
2024-12-14 20:47:54 +0800

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,16 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-config.color_scheme = "Catppuccin Mocha"
+
+-- config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Tokyo Night"
+config.freetype_load_target = "Light"
 config.enable_tab_bar = false
+config.enable_scroll_bar = true
+config.dpi = 144.0
 config.font = wezterm.font("Hack Nerd Font")
 -- config.font = wezterm.font("MesloLGS Nerd Font")
-config.font_size = 15
+config.font_size = 9
 config.keys = {
 	{
 		key = "w",
@@ -15,3 +20,4 @@ config.keys = {
 }
 
 return config
+
