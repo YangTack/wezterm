924adaafad27e832d1b1b9ac5fadbfacf923b371
PeterYang <30444059+YangTack@users.noreply.github.com>
2024-12-16 09:55:01 +0800

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,22 +2,22 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
--- config.color_scheme = "Catppuccin Mocha"
-config.color_scheme = "Tokyo Night"
-config.freetype_load_target = "Light"
-config.enable_tab_bar = false
-config.enable_scroll_bar = true
-config.dpi = 144.0
-config.font = wezterm.font("Hack Nerd Font")
--- config.font = wezterm.font("MesloLGS Nerd Font")
-config.font_size = 9
-config.keys = {
-	{
-		key = "w",
-		mods = "SUPER",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	default_cursor_style = "BlinkingBar",
+	color_scheme = "Tokyo Night",
+	font = wezterm.font("Hack Nerd Font"),
+	font_size = 13.5,
+	dpi = 144.0,
+	keys = {
+		{
+			key = "w",
+			mods = "SUPER",
+			action = wezterm.action.CloseCurrentPane({ confirm = true }),
+		},
 	},
 }
 
 return config
-
